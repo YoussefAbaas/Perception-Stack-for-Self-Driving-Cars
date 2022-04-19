2a0738b21443e2bb0cadd695c1b9d355f125cf48
Waleed <76116605+waleedabdelaty@users.noreply.github.com>
2022-04-20 01:29:07 +0200

    Update lanes_project.py

--- lanes_project.py
+++ lanes_project.py
@@ -5,6 +5,20 @@ from moviepy.editor import VideoFileClip
 import os
 import sys
 
+def canny_edge_detector(img):
+    # Convert to HLS color space and separate the V channel
+    hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+    hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+    l_channel = hls[:,:,1]
+    s_channel = hls[:,:,2]
+    h_channel = hls[:,:,0]
+    v_channel=  hsv[:,:,2]
+    _,v_channel=cv2.threshold(v_channel,190, 255, cv2.THRESH_BINARY_INV)
+    edged1=cv2.Canny(s_channel,50,150)
+    edged2=cv2.Canny(v_channel,50,150)
+    edged=edged1 | edged2
+    return edged
+
 def perspective_warp(img):
     dst_size=(img.shape[1],img.shape[0])
     src=np.float32([(0.43,0.65),(0.58,0.65),(0.1,1),(1,1)])
@@ -30,6 +44,35 @@ def inv_perspective_warp(img):
     # Warp the image using OpenCV warpPerspective()
     warped = cv2.warpPerspective(img, M, dst_size)
     return warped
+
+def get_curve(img, leftx, rightx):
+    # Generate y values for plotting
+    ploty = np.linspace(0, img.shape[0]-1, img.shape[0])
+    y_eval = np.max(ploty)
+    # calculate meters per pixel (field of view in meters / image width)
+    ym_per_pix = 30.5/720 # meters per pixel in y dimension 
+    xm_per_pix = 3.7/720 # meters per pixel in x dimension
+
+    # Fit new polynomials to x,y in world space
+    left_fit_cr = np.polyfit(ploty*ym_per_pix, leftx*xm_per_pix, 2)
+    right_fit_cr = np.polyfit(ploty*ym_per_pix, rightx*xm_per_pix, 2)
+    # Calculate the new radii of curvature
+    left_curverad = ((1 + (2*left_fit_cr[0]*y_eval*ym_per_pix + left_fit_cr[1])**2)**1.5) / np.absolute(2*left_fit_cr[0])
+    right_curverad = ((1 + (2*right_fit_cr[0]*y_eval*ym_per_pix + right_fit_cr[1])**2)**1.5) / np.absolute(2*right_fit_cr[0])
+    
+    #calculate car position
+    car_pos = img.shape[1]/2
+
+    #calculate lane center 
+    l_fit_x_int = left_fit_cr[0]*img.shape[0]**2 + left_fit_cr[1]*img.shape[0] + left_fit_cr[2]
+    r_fit_x_int = right_fit_cr[0]*img.shape[0]**2 + right_fit_cr[1]*img.shape[0] + right_fit_cr[2]
+    lane_center_position = (r_fit_x_int + l_fit_x_int) /2
+
+    center = (car_pos - lane_center_position) * xm_per_pix / 10
+    # Now our radius of curvature is in meters
+    return (left_curverad, right_curverad, center)
+
+
 
 def draw_lanes(img, left_fit, right_fit):
     ploty = np.linspace(0, img.shape[0]-1, img.shape[0])
